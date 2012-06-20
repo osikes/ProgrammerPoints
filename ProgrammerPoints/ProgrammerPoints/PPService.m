93db502209ef497135d35b3535e6c2ee83b81f5f
Robert Smyly <robertsmyly@gmail.com>
2012-06-20 12:39:20 -0500

    started object mapping for restkit

--- ProgrammerPoints/ProgrammerPoints/PPService.m
+++ ProgrammerPoints/ProgrammerPoints/PPService.m
@@ -9,6 +9,10 @@
 #import "PPService.h"
 #import <RestKit/RestKit.h>
 
+#import "PPPoint.h"
+#import "PPCollection.h"
+#import "PPIdiot.h"
+
 @implementation PPService
 
 - (id) initWithBaseURL:(NSString *) baseURL {
@@ -16,6 +20,28 @@
     if (self != nil) {
         RKClient* client = [RKClient clientWithBaseURL:baseURL]; //initialize RKClient with base url
     }
+    
+    RKObjectMapping* pointMapping = [RKObjectMapping mappingForClass:[PPPoint class]];
+    [pointMapping mapKeyPath:@"Id" toAttribute:@"id"];
+    [pointMapping mapKeyPath:@"date" toAttribute:@"date"];
+    [pointMapping mapKeyPath:@"description" toAttribute:@"description"];
+    [pointMapping mapKeyPath:@"value" toAttribute:@"value"];
+    //[[RKObjectManager sharedManager].mappingProvider setMapping:pointMapping forKeyPath:@"points"];
+    
+    RKObjectMapping* collectionMapping = [RKObjectMapping mappingForClass:[PPCollection class]];
+    [collectionMapping mapKeyPath:@"Id" toAttribute:@"id"];
+    [collectionMapping mapKeyPath:@"total" toAttribute:@"total"];
+    [collectionMapping mapKeyPath:@"points" toAttribute:@"points"];
+    [[RKObjectManager sharedManager].mappingProvider setMapping:collectionMapping forKeyPath:@"smartPoints"];
+    [[RKObjectManager sharedManager].mappingProvider setMapping:collectionMapping forKeyPath:@"stupidPoints"];
+    
+    RKObjectMapping* idiotMapping = [RKObjectMapping mappingForClass:[PPIdiot class]];
+    [idiotMapping mapKeyPath:@"Id" toAttribute:@"id"];
+    [idiotMapping mapKeyPath:@"firstName" toAttribute:@"firstName"];
+    [idiotMapping mapKeyPath:@"lastName" toAttribute:@"lastName"];
+    [idiotMapping mapKeyPath:@"smartPoints" toAttribute:@"smartPoints"];
+    [idiotMapping mapKeyPath:@"stupidPoints" toAttribute:@"stupidPoints"];
+    
     return self;
 }
 
